7633b481aaaf171ae0d16dce34c162b50c2f8d12
Anant <anant.asty@gmail.com>
2016-11-16 18:33:16 -0700

    added defaults

--- telegraf/1.1/entrypoint.sh
+++ telegraf/1.1/entrypoint.sh
@@ -3,8 +3,10 @@ set -e
 
 if [ -n "$CONFIG_FILE" ]; then
     wget -O /tmp/telegraf.conf "$CONFIG_FILE"
-    mo /tmp/telegraf.conf > /etc/telegraf/telegraf.conf
+else
+    wget -O /tmp/telegraf.conf "https://raw.githubusercontent.com/anantasty/influxdata-docker/master/telegraf/1.1/telegraf.conf"
 fi
+mo /tmp/telegraf.conf > /etc/telegraf/telegraf.conf
 
 if [ "${1:0:1}" = '-' ]; then
     set -- telegraf "$@"

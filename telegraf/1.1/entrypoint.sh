e2e6ed65678b31cb4f49a287104b227ade0e29d9
Anant <anant.asty@gmail.com>
2016-11-16 22:36:16 -0700

    added debug echo to see the config file

--- telegraf/1.1/entrypoint.sh
+++ telegraf/1.1/entrypoint.sh
@@ -7,6 +7,8 @@ else
     wget -O /tmp/telegraf.conf "https://raw.githubusercontent.com/anantasty/influxdata-docker/master/telegraf/1.1/telegraf.conf"
 fi
 mo /tmp/telegraf.conf > /etc/telegraf/telegraf.conf
+echo "CONFIG FILE IS"
+cat /etc/telegraf/telegraf.conf
 
 if [ "${1:0:1}" = '-' ]; then
     set -- telegraf "$@"
